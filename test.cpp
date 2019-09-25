59f0e2d8ac2dcc50e24c170741f259e4f7305232
Christos Chatzivagias <cchatzivagias@beta-cae.com>
2019-09-25 18:58:02 +0300

    Add gtest

--- test.cpp
+++ test.cpp
@@ -3,7 +3,14 @@
 #include "cpuMergesort.h"
 
 TEST(AddTest, Test){
-	ASSERT_EQ(4,4);
+	int rightsorted[10] =  {2,2,2,5,5,6,6,7,10,34};
+	int unsorted[10] = {10,2,5,6,2,34,2,5,6,7};
+	int testsorted[10];
+	mergesort_cpu(unsorted, testsorted, 10);
+	
+	for(int index = 0; index < 10; index++) {
+		ASSERT_EQ(rightsorted[index], testsorted[index]);
+	}
 }
 
 int main(int argc, char *argv[]) {
